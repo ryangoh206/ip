241152134582977cc6b45c9cb50fb6162db543a5
Ryan Goh <ryangoh206@gmail.com>
2026-01-22 23:08:37 +0800

    Add support for echo in Grump.java

--- src/main/java/Grump.java
+++ src/main/java/Grump.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Grump {
     public static void main(String[] args) {
         String logo = "  ____ ____  _   _ __  __ ____  \n"
@@ -10,8 +12,21 @@ public class Grump {
         System.out.println("____________________________________________________________");
 
         System.out.println("Hello! I'm Grump\nWhat can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+    
+        // Loop and read commands from standard input until 'bye' is encountered
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("____________________________________________________________\n");
+            String userInput = scanner.nextLine().trim();
+            System.out.println("____________________________________________________________");
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println(userInput);
+            }
+        }
+        scanner.close();
     }
 }
