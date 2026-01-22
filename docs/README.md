f3795e1819d381937c28aa14e4767602f5c175ce
Ryan Goh <ryangoh206@gmail.com>
2026-01-22 21:07:36 +0800

    Rename main class and add greeting

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Grump User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Grump.java
@@ -0,0 +1,17 @@
+public class Grump {
+    public static void main(String[] args) {
+        String logo = "  ____ ____  _   _ __  __ ____  \n"
+            + " / ___|  _ \\| | | |  \\/  |  _ \\ \n"
+            + "| |  _| |_) | | | | |\\/| | |_) |\n"
+            + "| |_| |  _ <| |_| | |  | |  __/ \n"
+            + " \\____|_| \\_\\\\___/|_|  |_|_|    \n";
+        System.out.println("____________________________________________________________\n");
+        System.out.println(logo);
+        System.out.println("____________________________________________________________");
+
+        System.out.println("Hello! I'm Grump\nWhat can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
