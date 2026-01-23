9dad6438bb014a207fddcb1dcd70623b0b77d1bf
Ryan Goh <ryangoh206@gmail.com>
2026-01-23 11:56:10 +0800

    Add support for list in Grump.java
    Add an ArrayList to store tasks and printTasks method to loop through
    the ArrayList and print all the tasks with numbering from 1.

--- src/main/java/Grump.java
+++ src/main/java/Grump.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Grump {
+    private static ArrayList<String> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         String logo = "  ____ ____  _   _ __  __ ____  \n"
-            + " / ___|  _ \\| | | |  \\/  |  _ \\ \n"
-            + "| |  _| |_) | | | | |\\/| | |_) |\n"
-            + "| |_| |  _ <| |_| | |  | |  __/ \n"
-            + " \\____|_| \\_\\\\___/|_|  |_|_|    \n";
+                + " / ___|  _ \\| | | |  \\/  |  _ \\ \n"
+                + "| |  _| |_) | | | | |\\/| | |_) |\n"
+                + "| |_| |  _ <| |_| | |  | |  __/ \n"
+                + " \\____|_| \\_\\\\___/|_|  |_|_|    \n";
         System.out.println("____________________________________________________________\n");
         System.out.println(logo);
         System.out.println("____________________________________________________________");
 
         System.out.println("Hello! I'm Grump\nWhat can I do for you?");
-    
-        // Loop and read commands from standard input until 'bye' is encountered
+
+        // Loop, read commands from stdin and add to tasks until 'bye'
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("____________________________________________________________\n");
@@ -23,10 +26,20 @@ public class Grump {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (userInput.equals("list")) {
+                printTasks();
             } else {
-                System.out.println(userInput);
+                tasks.add(userInput);
+                System.out.println("added: " + userInput);
             }
         }
         scanner.close();
+    }
+
+    // Print all tasks with numbering
+    public static void printTasks() {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
+        }
     }
 }
